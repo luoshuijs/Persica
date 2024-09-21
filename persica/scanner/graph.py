578064e81e418da5ec7abb366f9a3f0354909fd5
OPaimon <paimoe@bupt.edu.cn>
2024-09-21 11:56:41 +0800

    :sparkles: Add a func to find the sink of the graph

--- persica/scanner/graph.py
+++ persica/scanner/graph.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Set
+from typing import Any, Dict, Iterable, List, Set
 
 import networkx as nx
 
@@ -8,8 +8,11 @@ class ClassGraph:
         self.graph = nx.DiGraph()
 
     def add_class(self, class_name: str, parent_names: Set[str]):
-        for parent in parent_names:
-            self.graph.add_edge(parent, class_name)
+        if parent_names:
+            for parent in parent_names:
+                self.graph.add_edge(parent, class_name)
+        else:
+            self.graph.add_node(class_name)
 
     def find_all_ancestors(self, class_name: str) -> Set[str]:
         return nx.ancestors(self.graph, class_name)
@@ -19,3 +22,10 @@ class ClassGraph:
 
     def get_class_info(self, class_name: str) -> Dict[str, Any]:
         return {"ancestors": self.find_all_ancestors(class_name), "descendants": self.find_all_descendants(class_name)}
+
+    def find_all_sink(self) -> List[str]:
+        out_degree = self.graph.out_degree()
+        if isinstance(out_degree, Iterable):
+            return [node for node, out_degree in out_degree if out_degree == 0]
+        else:
+            raise TypeError("out_degree is not iterable")
