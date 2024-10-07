fef4d18828480976778cabdd0e51b46bee453a87
LuoShui <luoshuijs@outlook.com>
2024-10-07 17:47:29 +0800

    :sparkles: Add Test ApplicationContext

---
+++ tests/context/test_application.py
@@ -0,0 +1,11 @@
+import asyncio
+
+
+class TestApplicationContext:
+
+    async def test_initialize_and_shutdown(self, app):
+        loop = asyncio.get_event_loop()
+        context = app.context
+        await loop.run_in_executor(None, context.run)
+        await context.initialize()
+        await context.shutdown()
